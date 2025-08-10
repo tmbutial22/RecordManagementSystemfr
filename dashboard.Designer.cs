167cee46c2ae0a0825e6c376044beb64b7dd9e17
Trisha Mae Macapuno <butialtrishamae22@gmail.com>
2025-08-10 19:25:17 +0800

    dashboard na hindi naka dash

---
+++ dashboard.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace RecordManagementSystemfr
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnprofile = new System.Windows.Forms.Button();
+            this.btndocu = new System.Windows.Forms.Button();
+            this.btnvisitor = new System.Windows.Forms.Button();
+            this.panelsched = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnscheds = new System.Windows.Forms.Button();
+            this.btnevents = new System.Windows.Forms.Button();
+            this.btngarbage = new System.Windows.Forms.Button();
+            this.btnouncement = new System.Windows.Forms.Button();
+            this.btnmonthly = new System.Windows.Forms.Button();
+            this.btndashb = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnlogout = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panelsched.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.iconPictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1257, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("iconPictureBox1.BackgroundImage")));
+            this.iconPictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 58;
+            this.iconPictureBox1.Location = new System.Drawing.Point(35, 10);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(159, 58);
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.btnlogout);
+            this.panel2.Controls.Add(this.btnprofile);
+            this.panel2.Controls.Add(this.btndocu);
+            this.panel2.Controls.Add(this.btnvisitor);
+            this.panel2.Controls.Add(this.panelsched);
+            this.panel2.Controls.Add(this.btnouncement);
+            this.panel2.Controls.Add(this.btnmonthly);
+            this.panel2.Controls.Add(this.btndashb);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 79);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(227, 723);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnprofile
+            // 
+            this.btnprofile.BackColor = System.Drawing.Color.Transparent;
+            this.btnprofile.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnprofile.BackgroundImage")));
+            this.btnprofile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnprofile.FlatAppearance.BorderSize = 0;
+            this.btnprofile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnprofile.Location = new System.Drawing.Point(0, 570);
+            this.btnprofile.Name = "btnprofile";
+            this.btnprofile.Size = new System.Drawing.Size(227, 53);
+            this.btnprofile.TabIndex = 4;
+            this.btnprofile.UseVisualStyleBackColor = false;
+            // 
+            // btndocu
+            // 
+            this.btndocu.BackColor = System.Drawing.Color.Transparent;
+            this.btndocu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btndocu.BackgroundImage")));
+            this.btndocu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btndocu.FlatAppearance.BorderSize = 0;
+            this.btndocu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndocu.Location = new System.Drawing.Point(-1, 506);
+            this.btndocu.Name = "btndocu";
+            this.btndocu.Size = new System.Drawing.Size(227, 53);
+            this.btndocu.TabIndex = 3;
+            this.btndocu.UseVisualStyleBackColor = false;
+            // 
+            // btnvisitor
+            // 
+            this.btnvisitor.BackColor = System.Drawing.Color.Transparent;
+            this.btnvisitor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnvisitor.BackgroundImage")));
+            this.btnvisitor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnvisitor.FlatAppearance.BorderSize = 0;
+            this.btnvisitor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnvisitor.Location = new System.Drawing.Point(-1, 442);
+            this.btnvisitor.Name = "btnvisitor";
+            this.btnvisitor.Size = new System.Drawing.Size(227, 53);
+            this.btnvisitor.TabIndex = 2;
+            this.btnvisitor.UseVisualStyleBackColor = false;
+            // 
+            // panelsched
+            // 
+            this.panelsched.BackColor = System.Drawing.Color.Transparent;
+            this.panelsched.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panelsched.Controls.Add(this.btnscheds);
+            this.panelsched.Controls.Add(this.btnevents);
+            this.panelsched.Controls.Add(this.btngarbage);
+            this.panelsched.Location = new System.Drawing.Point(-1, 375);
+            this.panelsched.MaximumSize = new System.Drawing.Size(228, 182);
+            this.panelsched.MinimumSize = new System.Drawing.Size(228, 58);
+            this.panelsched.Name = "panelsched";
+            this.panelsched.Size = new System.Drawing.Size(228, 58);
+            this.panelsched.TabIndex = 2;
+            this.panelsched.Paint += new System.Windows.Forms.PaintEventHandler(this.panelscheds_Paint);
+            // 
+            // btnscheds
+            // 
+            this.btnscheds.BackColor = System.Drawing.Color.Transparent;
+            this.btnscheds.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnscheds.BackgroundImage")));
+            this.btnscheds.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnscheds.FlatAppearance.BorderSize = 0;
+            this.btnscheds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnscheds.Location = new System.Drawing.Point(3, 3);
+            this.btnscheds.Name = "btnscheds";
+            this.btnscheds.Size = new System.Drawing.Size(227, 53);
+            this.btnscheds.TabIndex = 3;
+            this.btnscheds.UseVisualStyleBackColor = false;
+            this.btnscheds.Click += new System.EventHandler(this.btnscheds_Click_1);
+            // 
+            // btnevents
+            // 
+            this.btnevents.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnevents.BackgroundImage")));
+            this.btnevents.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnevents.FlatAppearance.BorderSize = 0;
+            this.btnevents.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnevents.Location = new System.Drawing.Point(3, 62);
+            this.btnevents.Name = "btnevents";
+            this.btnevents.Size = new System.Drawing.Size(227, 53);
+            this.btnevents.TabIndex = 2;
+            this.btnevents.UseVisualStyleBackColor = true;
+            // 
+            // btngarbage
+            // 
+            this.btngarbage.BackColor = System.Drawing.Color.Transparent;
+            this.btngarbage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btngarbage.BackgroundImage")));
+            this.btngarbage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btngarbage.FlatAppearance.BorderSize = 0;
+            this.btngarbage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btngarbage.Location = new System.Drawing.Point(3, 121);
+            this.btngarbage.Name = "btngarbage";
+            this.btngarbage.Size = new System.Drawing.Size(227, 53);
+            this.btngarbage.TabIndex = 2;
+            this.btngarbage.UseVisualStyleBackColor = false;
+            // 
+            // btnouncement
+            // 
+            this.btnouncement.BackColor = System.Drawing.Color.Transparent;
+            this.btnouncement.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnouncement.BackgroundImage")));
+            this.btnouncement.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnouncement.FlatAppearance.BorderSize = 0;
+            this.btnouncement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnouncement.Location = new System.Drawing.Point(-1, 314);
+            this.btnouncement.Name = "btnouncement";
+            this.btnouncement.Size = new System.Drawing.Size(227, 53);
+            this.btnouncement.TabIndex = 2;
+            this.btnouncement.UseVisualStyleBackColor = false;
+            // 
+            // btnmonthly
+            // 
+            this.btnmonthly.BackColor = System.Drawing.Color.Transparent;
+            this.btnmonthly.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnmonthly.BackgroundImage")));
+            this.btnmonthly.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnmonthly.FlatAppearance.BorderSize = 0;
+            this.btnmonthly.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnmonthly.Location = new System.Drawing.Point(0, 251);
+            this.btnmonthly.Name = "btnmonthly";
+            this.btnmonthly.Size = new System.Drawing.Size(227, 53);
+            this.btnmonthly.TabIndex = 1;
+            this.btnmonthly.UseVisualStyleBackColor = false;
+            // 
+            // btndashb
+            // 
+            this.btndashb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btndashb.BackgroundImage")));
+            this.btndashb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btndashb.FlatAppearance.BorderSize = 0;
+            this.btndashb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndashb.Location = new System.Drawing.Point(0, 188);
+            this.btndashb.Name = "btndashb";
+            this.btndashb.Size = new System.Drawing.Size(227, 53);
+            this.btndashb.TabIndex = 0;
+            this.btndashb.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnlogout
+            // 
+            this.btnlogout.BackColor = System.Drawing.Color.Transparent;
+            this.btnlogout.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnlogout.BackgroundImage")));
+            this.btnlogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnlogout.FlatAppearance.BorderSize = 0;
+            this.btnlogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogout.Location = new System.Drawing.Point(0, 670);
+            this.btnlogout.Name = "btnlogout";
+            this.btnlogout.Size = new System.Drawing.Size(227, 53);
+            this.btnlogout.TabIndex = 5;
+            this.btnlogout.UseVisualStyleBackColor = false;
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 802);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dashboard";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panelsched.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btndashb;
+        private System.Windows.Forms.Button btnmonthly;
+        private System.Windows.Forms.Button btnouncement;
+        private System.Windows.Forms.Button btnscheds;
+        private System.Windows.Forms.FlowLayoutPanel panelsched;
+        private System.Windows.Forms.Button btnevents;
+        private System.Windows.Forms.Button btngarbage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnvisitor;
+        private System.Windows.Forms.Button btndocu;
+        private System.Windows.Forms.Button btnprofile;
+        private System.Windows.Forms.Button btnlogout;
+    }
+}
---
+++ dashboard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecordManagementSystemfr
+{
+    public partial class dashboard : Form
+    {
+        private bool isCollapsed;
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScheds_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelscheds_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCollapsed)
+            {
+                panelsched.Height += 10;
+                if (panelsched.Size == panelsched.MaximumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = false;
+                }
+            }
+            else
+            {
+                panelsched.Height -= 10;
+                if (panelsched.Size == panelsched.MinimumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = true;
+                }
+
+            }
+        }
+
+        private void btnscheds_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
